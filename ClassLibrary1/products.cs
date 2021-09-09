1a64033fed537c592e3f6c6ba0a87b8ab24e8674
Fidel Makatia <fidel.makatia@autodesk.com>
2021-09-09 09:10:24 +0300

    Add project files.

---
+++ ClassLibrary1/products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ECommerce
+{
+    public class Products
+    {
+
+        public int ID { get; set; }
+
+        public string name { get; set; }
+
+        public int price { get; set; }
+
+        public DateTime DateofManufacture { get; set; }
+    }
+}
---
+++ Collection of objects/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ECommerce;
+
+using System.Collections.Generic;
+
+
+namespace Collection_of_objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Products> myproducts = new List<Products>();
+
+
+            string choice;
+
+            do
+            {
+
+                Console.WriteLine("Enter product ID");
+                int pid = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter product Name");
+                string pname = Console.ReadLine();
+                Console.WriteLine("Enter product Price");
+                int pprice = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter product Manufacture Date(yyyy-MM-dd");
+                DateTime pdate = DateTime.Parse(Console.ReadLine());
+
+
+                //create object of class product
+                Products product = new Products() { ID = pid, name = pname, price = pprice, DateofManufacture = pdate };
+
+                //add to th list
+
+                myproducts.Add(product);
+                Console.WriteLine("products added");
+                Console.WriteLine("Do you wish to continue? (Yes/No)");
+                choice= Console.ReadLine();
+
+            } while (choice != "No" && choice != "NO" && choice != "no" && choice != "N" && choice != "n");
+
+
+            //foreach
+            Console.WriteLine("\n*******************Products:*****************************");
+            foreach(Products item in myproducts)
+            {
+                Console.WriteLine(item.ID + ". " + item.name + ". " + item.price + "." + item.DateofManufacture.ToShortDateString());
+            }
+        }
+    }
+}
